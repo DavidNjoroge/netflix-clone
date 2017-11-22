ac087779b7614ac6dc31fc78eae50cfe8a08ea31
David Mwangi <njorogedavid13@gmail.com>
2017-11-22 14:21:37 +0300

    add mockups

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/chutha/Documents/moringa-school-project/Moringa Core/python/netflix_clone/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
